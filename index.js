c25351d402edd68f14bde033db53b0af7621aff4
Sarah Burgess <sarahburgess287@gmail.com>
2018-10-23 21:16:21 -0700

    Added pseudocode and sample queries to index.js, removed most of static content from index.html, adjusted classes on the style.html and index.html

--- index.js
+++ index.js
@@ -1,23 +1,102 @@
 'use strict';
 
+
+// this function runs on click of the Nebulas button
+function showNebulasSearchPage() {
+
+}
+
+// this function runs on click fo the stars button
+function showStarSearchPage() {
+
+}
+
+// this function runs on click of the planet button
+function showPlanetsSearchPage() {
+
+}
+
+
+
+// Function that returns the html to load on the welcome page
+
+// Dynamically load the html of the welcome page when the page loads
+function showWelcomePage(){
+  console.log('showWelcomePage ran');
+}
+
+
+
+
+
 function handleForm() {
     console.log('handleForm ran');
+    // Show welcome page
+    showWelcomePage();
+
+    // On click of planets button, load the search planet page
+
+    // On click of stars button, load the stars page
+
+    // On click of the Nebulas button, load the nebulas page
+
+    // Listen for the form submit on each page
+    // Pass the value of the form to nasa api query
+    // Display results (this may be part of the function above to pass the value of form to nasa api)
+
+    // var settings = {
+    //   "async": true,
+    //   "crossDomain": true,
+    //   "url": "https://cors-anywhere.herokuapp.com/https://images-api.nasa.gov/search?media_type=image&q=Neptune",
+    //   "method": "GET",
+    //   "headers": {
+    //     "api_key": "MQP3dZedmR0tH2NUAMuRTva9WKV47YnTwF1mbumk",
+    //     "keywords": "planet",
+    //     "title": "Neptune",
+    //     "cache-control": "no-cache",
+    //     "postman-token": "ca0cae98-87ec-0b79-730a-2246038d0e76",
+    //     "X-Requested-With" : "XMLHttpRequest"
+    //   }
+    // }
+    
+    // $.ajax(settings).done(function (response) {
+    //   console.log(response);
+    // });
+
+    // returns just a couple results
+    // var settings = {
+    //   "async": true,
+    //   "crossDomain": true,
+    //   "url": "https://cors-anywhere.herokuapp.com/https://images-api.nasa.gov/search?media_type=image&q=Betelgeuse",
+    //   "method": "GET",
+    //   "headers": {
+    //     "api_key": "MQP3dZedmR0tH2NUAMuRTva9WKV47YnTwF1mbumk",
+    //     "cache-control": "no-cache",
+    //     "postman-token": "1c3e853b-9041-3e80-6706-1673889f947b",
+    //     "X-Requested-With" : "XMLHttpRequest"
+    //   }
+    // }
+    
+    // $.ajax(settings).done(function (response) {
+    //   console.log(response);
+    // });
+   
 
     // // Sample query from: https://api.nasa.gov/planetary/apod?api_key=MQP3dZedmR0tH2NUAMuRTva9WKV47YnTwF1mbumk
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://api.nasa.gov/planetary/apod?api_key=MQP3dZedmR0tH2NUAMuRTva9WKV47YnTwF1mbumk",
-        "method": "GET",
-        "headers": {
-          "cache-control": "no-cache",
-          "postman-token": "16b06f60-911f-b377-7af0-909378870dff"
-        }
-      }
+    // var settings = {
+    //     "async": true,
+    //     "crossDomain": true,
+    //     "url": "https://api.nasa.gov/planetary/apod?api_key=MQP3dZedmR0tH2NUAMuRTva9WKV47YnTwF1mbumk",
+    //     "method": "GET",
+    //     "headers": {
+    //       "cache-control": "no-cache",
+    //       "postman-token": "16b06f60-911f-b377-7af0-909378870dff"
+    //     }
+    //   }
       
-      $.ajax(settings).done(function (response) {
-        console.log(response);
-      });
+    //   $.ajax(settings).done(function (response) {
+    //     console.log(response);
+    //   });
 
 }
 
@@ -34,3 +113,4 @@ $(handleForm);
 //   "title": "Meteor, Comet, and Seagull (Nebula)",
 //   "url": "https://apod.nasa.gov/apod/image/1810/CometMeteorNebula_TSam_1080.jpg"
 // }
+
